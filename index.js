7bf8ab40d72f86d9ff6289c9643fb342b6f2470c
Armando Ruiz <armando.ruiz@industrie.co>
2022-03-28 15:14:24 +1100

    20. Instalando Pug y habilitando las vistas en express

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const express = require('express');
 // para importar las rutas
 const routes = require('./routes');
 
+//para leer las vistas y los archivos
+const path = require('path');
+
 // crear una applicacion de express para el servidor
 const app = express();
+
+
+// Habilitar Pug
+app.set('view engine', 'pug');
+// Ageregar la carpeta de las vistas
+app.set('views', path.join(__dirname, './views'));
 
 // ruta para el home es un middleware
 // app.use('/', (req, res) => {
