71c36eb887b4b26c66b5444b6202a0bbd874c06b
Armando Ruiz <armando.ruiz@industrie.co>
2022-03-28 15:58:33 +1100

    23. Creando la seccion para nuevos proyectos

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,7 @@ module.exports = function() {
   // ruta para el home
   router.get('/', proyectosController.proyectosHome);
 
+  router.get('/nuevo-projecto', proyectosController.formularioProyecto);
+
   return router;
 }
