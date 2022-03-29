6deb90924df5297b5b6216b3373ad5e1215b893f
Armando Ruiz <armando.ruiz@industrie.co>
2022-03-30 08:58:39 +1100

    39 y 40, generando url del proyecto usando slugs con hooks

--- models/Proyectos.js
+++ models/Proyectos.js
@@ -3,18 +3,27 @@ const Sequelize = require('sequelize');
 // importamos configuracion de la conexion de la BD
 const db = require('../config/db');
 
+// importamos slug para la url
+const slug = require('slug');
+
 // definimos el modelo
 // lo 1 q pasamos es el nombre de la tabla proyectos
 // lo 2 pasamos las columnas
-const Proyectos = db.define('proyectos',{
+const Proyectos = db.define('proyectos', {
   id : {
       type: Sequelize.INTEGER,
       primaryKey: true,
       autoIncrement: true
   },
-
-  nombre : Sequelize.STRING,
-  url : Sequelize.STRING
+  nombre :  Sequelize.STRING(100),
+  url : Sequelize.STRING(100)
+}, {
+    hooks: {
+      beforeCreate(proyecto) {
+        const url = slug(proyecto.nombre).toLowerCase();
+        proyecto.url = url;
+      }
+    }
 });
 
 module.exports = Proyectos;
